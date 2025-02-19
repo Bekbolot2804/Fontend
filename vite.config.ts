9c5b4a1ebb2627fb627db7a8ea6e9ab424904867
Бекболот <82055231+Bekbolot2804@users.noreply.github.com>
2025-02-19 23:03:38 +0300

    начало

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // server: { port: 3000 },
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/proxy": {
+        target: "http://localhost:8080/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, "/"),
+      },
+    },
+  },
+})
