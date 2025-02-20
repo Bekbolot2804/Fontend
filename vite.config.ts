02cf694c67b2fadd77f5cbe9faf82200995b6955
Бекболот <82055231+Bekbolot2804@users.noreply.github.com>
2025-02-20 16:44:49 +0300

    УРА! У меня работает Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Frontend/', // Укажите название вашего репозитория
+  base: './', // Укажите название вашего репозитория
   server: {
     port: 3000,
     proxy: {
