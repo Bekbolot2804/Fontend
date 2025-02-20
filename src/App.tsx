36e8f8743215fb8c83ccdae78844c47299418673
Бекболот <82055231+Bekbolot2804@users.noreply.github.com>
2025-02-20 12:45:45 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-//import Head from "./modules/Head";
 import SpaceObjectsPage from "./pages/SpaceObjectsPage";
 import SpaceObjectDetailPage from "./pages/SpaceObjectDetailPage"
 import { HomePage } from "./pages/HomePage.tsx";
-//import Foot from "./modules/Foot";
 import { ROUTES } from "./Routes";
 import './App.css'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    // <React.Fragment>
-    //   {/*<Head></Head>*/}
-    //   <SpaceObjectsPage></SpaceObjectsPage>
-    //   {/*<Foot></Foot>*/}
-    // </React.Fragment>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path={ROUTES.HOME} index element={<HomePage />} />
         <Route path={ROUTES.SPACEOBJECTS} element={<SpaceObjectsPage />} />
         <Route path={`${ROUTES.SPACEOBJECTS}/:id`} element={<SpaceObjectDetailPage />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  // server: { port: 3000 },
   plugins: [react()],
+  base: '/Frontend/', // Укажите название вашего репозитория
   server: {
     port: 3000,
     proxy: {
@@ -15,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
